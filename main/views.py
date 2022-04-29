2225a31ef0dd01927b413399891918c4032baa1e
unknown <ash1313@koreatech.ac.kr>
2022-04-29 17:19:46 +0900

    2

--- main/views.py
+++ main/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
-# Create your views here.
+
 def signup(request):
-    return render(request, "signup.html")
+    if request.method =='GET':
+        return render(request,'signup.html')
+
+    elif request.method =='POST':
+        email = request.POST.get('email','')
+        nickname = request.POST.get('nickname','')
+        password1 = request.POST.get('password1','')
+        password2 = request.POST.get('password2','')
+        if(email or nickname or password1 or password2) =='':
+            return redirect('../signup/')
+        elif password1 != password2:
+            return redirect('../signup/')
+        else:
+            user = User(
+                email = email,
+                password = password1,
+                nickname = nickname,
+            )
+            user.save()
+        return redirect('../signin/')
+def signin(request):
+        return render(request,'signin.html')
+        
